9997e3d1badc8fb92fa84de0bb1fafe985fa8ff9
Valstan <123qwemmm@mail.ru>
2019-05-14 02:16:07 +0300

    Почти финал )))

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+# Проверяем введены ли аргументы
+if (( $# < 3 )); then
+echo "!ОШИБКА! Введен мало аргументов! Пример запуска скрипта:"
+echo "./deploy ftp://ftplogin:ftpassword@ftpaddress.com/dir zipfile.zip /var/www/webserver"
+exit
+fi
+
+# Присваиваем переменым значения
+NoDeVeRSioN="setup_10.x"
 uTiLiTeS="nano mc build-essential libssl-dev git cron socat curl libcurl3 openssl dirmngr"
-apt update
-apt upgrade -y
 
 # Утилиты
+apt update
+apt upgrade -y
 apt install -y $uTiLiTeS
 
 # NodeJS
@@ -12,24 +21,24 @@ curl -sL https://deb.nodesource.com/$NoDeVeRSioN | bash -
 apt install -y nodejs
 
 # MongoDB
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 apt update
 apt install -y mongodb-org
 service mongod start
 
 # Заливаем и распаковываем Фортуналог через FTP
-cd /$5
-rm $4.zip
-wget ftp://$1:$2@$3/$4.zip
-tar -xf $4.zip
-rm $4.zip
+mkdir -m u+x -p $3
+rm -rf $3/*
+cd $3
+wget $1/$2
+tar -xf $2 --strip-components 1
+rm $2
 
 # PM2
 npm install pm2@latest -g
 
 # Устанавливаем модули сервера
-cd /$5/$4
 npm install
 node postinstall.js
 
