40e10d91a75cf0835b4c397f6172b280729a08ac
Valstan <123qwemmm@mail.ru>
2019-05-14 15:47:21 +0300

    убрал sudo из монго

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ apt install -y nodejs
 
 # MongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 apt update
 apt install -y mongodb-org
 service mongod start
